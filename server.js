9e6a8e84c99820443b750a0c79dba85fd2bf85ef
ngyngcphu <nguyenngocphu225@gmail.com>
2023-03-14 10:30:09 +0700

    dev docker compose

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 const ronin = require('ronin-server');
 const mocks = require('ronin-mocks');
+const database = require('ronin-database');
 
-const server = ronin.server();
+async function main() {
+    try {
+        await database.connect(process.env.CONNECTIONSTRING);
+        
+        const server = ronin.server({
+            port: process.env.SERVER_PORT
+        });
 
-server.use('/', mocks.server(server.Router(), false, true));
-server.start();
+        server.use('/foo', (req, res) => {
+            return res.json({"foo": "bar"});
+        })
+        server.use('/', mocks.server(server.Router()));
+
+        const result = await server.start();
+        console.info(result);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+main();
